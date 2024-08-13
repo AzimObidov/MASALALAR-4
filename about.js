98a76638b890e0ff8db289561a4a0331f714bed3
AzimObidov <azimazimazim2009@gmail.com>
2024-08-13 07:26:40 +0500

    masalalar 4

---
+++ about.js
@@ -0,0 +1,328 @@
+// 1
+// let A = 5;
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(A);
+// }
+
+
+// 2
+// let a = 5
+// let b = 10
+// console.log(a,b);
+// for (;a < b; a ++ ){
+//     console.log(a)
+// }
+
+
+// 3
+// let a = 5
+// let b = 10
+// for (;a < b; b -- ){
+//     console.log(b)
+// }
+
+
+// 4
+// let a = 5
+
+// for(;10 > a; a++ ){
+//     console.log(a,'som');
+// }
+
+
+// 5
+// let narx = 20;
+
+// for (let i = 0.1; i <= 1; i += 0.1) {
+//     let narxi = narx* i;
+//     console.log(narx);
+// }
+
+
+// 6
+// let narx1 = 20;
+
+// for (let i = 0.1; i <= 1; i += 0.2) {
+//     let narxi = narx1Kg * i;
+//     console.log(i.toFixed(1));
+// }
+
+
+// 7
+// let a = 5;
+// let b = 2;
+
+// for(;a > b; b++){
+//     console.log(b a + a)
+// }
+
+
+// 8
+// let a = 5;
+// let b = 2;
+
+// for(;a > b; b++){
+//     console.log(b * 2)
+// }
+
+// 9
+// let a = 5;
+// let b = 2;
+
+// for(;a > b; b++){
+//     console.log(b * 4)
+// }
+
+
+// 10
+// function a(n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         b += 1 / i;
+//     }
+//     return b;
+// }
+
+// console.log(a(5));
+
+
+// 11
+// function a(n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         b += Math.pow(n + 1, i);
+//     }
+//     return b;
+// }
+
+// console.log(a(3));
+
+
+// 12
+// function a(n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             b -= i;
+//         } else {
+//             b += i;
+//         }
+//     }
+//     return b;
+// }
+
+// console.log(a(5));
+
+
+// 13
+// function a(n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             b -= i;
+//         } else {
+//             b += i;
+//         }
+//     }
+//     return b;
+// }
+
+// console.log(a(5)); 
+
+
+// 14
+// function a(n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         b += (2 * i - 1);
+//     }
+//     return b;
+// }
+
+// console.log(a(5));
+
+
+// 15
+// function s(a, n) {
+//     let result = 1;
+//     for (let i = 1; i <= n; i++) {
+//         result *= a;
+//     }
+//     return result;
+// }
+
+// console.log(s(2, 3));
+
+
+// 16
+// function s(a, n) {
+//     let b = '';
+//     for (let i = 1; i <= n; i++) {
+//         b += Math.pow(a, i) + ' ';
+//     }
+//     return b.trim();
+// }
+
+// console.log(s(2, 3));
+
+
+// 17
+// function s(a, n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         b += Math.pow(a, i);
+//     }
+//     return b;
+// }
+
+// console.log(s(2, 3));
+
+
+// 18
+// function s(a, n) {
+//     let b = 0;
+//     for (let i = 1; i <= n; i++) {
+//         b += Math.pow(-a, i);
+//     }
+//     return b;
+// }
+
+// console.log(s(2, 3));
+
+// 19
+// function a(n) {
+//     let b = 1;
+//     for (let i = 1; i <= n; i++) {
+//         b *= i;
+//     }
+//     return b;
+// }
+
+// console.log(a(5)); 
+
+
+// 20
+// function a(n) {
+//     let s = 0;
+//     let b = 1;
+//     for (let i = 1; i <= n; i++) {
+//         b *= i;
+//         s += b;
+//     }
+//     return s;
+// }
+
+// console.log(a(5));
+
+
+
+
+
+
+
+
+// 1
+// function s(a, b) {
+//     let w = 0;
+//     while (a >= b) {
+//         a -= b;
+//         w++;
+//     }
+//     return w;
+// }
+
+// console.log(s(20, 3));
+
+// 2
+// function w(a, b) {
+//     let s = 0;
+//     while (a >= b) {
+//         a -= b;
+//         s++;
+//     }
+//     return s;
+// }
+
+// console.log(w(20, 3));
+
+
+// 3
+// function w(a, b) {
+//     while (a >= b) {
+//         a -= b;
+//     }
+//     return a;
+// }
+
+// console.log(w(17, 5)); 
+
+
+// 4
+// function w(a) {
+//     while (a % 3 === 0 && a !== 0) {
+//         a /= 3;
+//     }
+//     return a === 1;
+// }
+
+// console.log(w(27));
+
+
+// 5
+// function w(a) {
+//     let b = 0;
+//     while (a % 2 === 0 && a !== 1) {
+//         a /= 2;
+//         b++;
+//     }
+//     return b;
+// }
+
+// console.log(w(16));
+
+
+// 6
+// function w(e) {
+//     let a = 1;
+//     let b = e;
+//     while (b > 0) {
+//         a *= b;
+//         b -= 2;
+//     }
+//     return a;
+// }
+
+// console.log(w(5));
+
+
+// 7
+// function w(e) {
+//     let a = 1;
+//     while (k * a <= e) {
+//         a++;
+//     }
+//     return a;
+// }
+
+// console.log(w(17));
+
+
+// 8
+// function w(e) {
+//     let a = 1;
+//     while (a * a <= e) {
+//         a++;
+//     }
+//     return a - 1;
+// }
+
+// console.log(w(17));
+
+
+// 9
+// !!!!
+
+
+// 10
+!!!
